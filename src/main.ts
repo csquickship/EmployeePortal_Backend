94b1e4ad9d152dd57c796356df9c0c67fa04b7a3
jatinsaini <jatinsaini@cssoftsolutions.com>
2025-12-02 10:15:25 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000,https://employee-portal-sandy.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
