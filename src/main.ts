fda10b7e50578f5ceff903fe38db86d5e502ca97
jatinsaini <jatinsaini@cssoftsolutions.com>
2025-12-02 10:18:09 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000,https://employee-portal-sandy.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://employee-portal-sandy.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
