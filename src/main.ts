54c991a57a0bbed9a8eda040a335e893585aa4df
jatinsaini <jatinsaini@cssoftsolutions.com>
2025-12-02 10:21:10 +0530

    cors upt

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://employee-portal-sandy.vercel.app',
-    ],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
